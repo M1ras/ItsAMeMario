3c998ced3a5dfd70b3ddd238af4c1af2f7e1ec19
Miras Absar <miras@miras.io>
2017-10-23 13:22:07 -0700

    Make Mario a little faster (sorry Luigi).

--- ItsAMeMario.sh
+++ ItsAMeMario.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/JoshuaKGoldberg/Old-Deleted-FullScreenMario ~/.fullScreenMario
+git clone --depth 8 https://github.com/JoshuaKGoldberg/Old-Deleted-FullScreenMario ~/.fullScreenMario
 cd ~/.fullScreenMario
 git reset --hard 77b493c9ce9fb015a88995658b11d95e28f1e68e
 xdg-open ./index.html
